1e849c8de1d75399cb2300cc3bdf79e04a3371c6
tuliorezende <tulio.rezende@outlook.com>
2026-01-29 18:30:17 -0300

    feat: Add Azure RM provider configuration and update README for SQL Server integration

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
 provider "mongodbatlas" {
   public_key  = var.mongodb_atlas_public_key
   private_key = var.mongodb_atlas_private_key
@@ -5,6 +13,10 @@ provider "mongodbatlas" {
 
 terraform {
   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.41.0"
+    }
     mongodbatlas = {
       source  = "mongodb/mongodbatlas"
       version = "~> 1.17"
